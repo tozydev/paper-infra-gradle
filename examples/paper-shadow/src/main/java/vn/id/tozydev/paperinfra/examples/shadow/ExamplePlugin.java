b41ad8ae54b2e76350f07074350abc6ee16b9fcb
Nguyễn Thanh Tân <me@tozydev.id.vn>
2025-06-21 15:50:06 +0700

    style: add .editorconfig and format

--- examples/paper-shadow/src/main/java/vn/id/tozydev/paperinfra/examples/shadow/ExamplePlugin.java
+++ examples/paper-shadow/src/main/java/vn/id/tozydev/paperinfra/examples/shadow/ExamplePlugin.java
@@ -1,21 +1,23 @@
 package vn.id.tozydev.paperinfra.examples.shadow;
 
-import kotlin.time.Clock;
-import org.bukkit.plugin.java.JavaPlugin;
-
 import static net.kyori.adventure.text.Component.text;
 import static net.kyori.adventure.text.format.NamedTextColor.*;
 
-public class ExamplePlugin extends JavaPlugin {
-    @Override
-    public void onEnable() {
-        getComponentLogger().info(text("Paper Infra Example Plugin enabled!", GREEN));
-        getComponentLogger().info(text("Current time: ", GRAY)
-                .append(text(Clock.System.INSTANCE.now().toString(), GOLD)));
-    }
+import kotlin.time.Clock;
+import org.bukkit.plugin.java.JavaPlugin;
 
-    @Override
-    public void onDisable() {
-        getComponentLogger().info(text("Paper Infra Example Plugin disabled!", RED));
-    }
+public class ExamplePlugin extends JavaPlugin {
+  @Override
+  public void onEnable() {
+    getComponentLogger().info(text("Paper Infra Example Plugin enabled!", GREEN));
+    getComponentLogger()
+        .info(
+            text("Current time: ", GRAY)
+                .append(text(Clock.System.INSTANCE.now().toString(), GOLD)));
+  }
+
+  @Override
+  public void onDisable() {
+    getComponentLogger().info(text("Paper Infra Example Plugin disabled!", RED));
+  }
 }
